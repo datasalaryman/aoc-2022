cd805498fdc3b277a061615608c0f74d9039459f
Data Salaryman <datasalaryman@protonmail.com>
2022-12-01 15:27:56 +0800

    fix(day-001): add fix for terminating case

--- day-001/one.py
+++ day-001/one.py
@@ -5,12 +5,17 @@ with open("day-001/input.txt", "r") as file:
 
 reindeer_collected = []
 one_reindeer = []
-for item in pz_input:
-    if item != '\n':
-        one_reindeer.append(int(item.rstrip("\n")))
-    else:
+for item_index in range(0, len(pz_input)):
+    if item_index == len(pz_input) - 1:
+        one_reindeer.append(int(pz_input[item_index].rstrip("\n")))
         reindeer_collected.append(one_reindeer)
         one_reindeer = []
+    elif pz_input[item_index] == "\n":
+        reindeer_collected.append(one_reindeer)
+        one_reindeer = []
+    else:
+        one_reindeer.append(int(pz_input[item_index].rstrip("\n")))
+
 
 max_reindeer = max([sum(reindeer) for reindeer in reindeer_collected])
 
--- day-001/two.py
+++ day-001/two.py
@@ -5,12 +5,16 @@ with open("day-001/input.txt", "r") as file:
 
 reindeer_collected = []
 one_reindeer = []
-for item in pz_input:
-    if item != '\n':
-        one_reindeer.append(int(item.rstrip("\n")))
-    else:
+for item_index in range(0, len(pz_input)):
+    if item_index == len(pz_input) - 1:
+        one_reindeer.append(int(pz_input[item_index].rstrip("\n")))
         reindeer_collected.append(one_reindeer)
         one_reindeer = []
+    elif pz_input[item_index] == "\n":
+        reindeer_collected.append(one_reindeer)
+        one_reindeer = []
+    else:
+        one_reindeer.append(int(pz_input[item_index].rstrip("\n")))
 
 reindeer_collected = [sum(collected) for collected in reindeer_collected]
 reindeer_collected.sort(reverse=True)
